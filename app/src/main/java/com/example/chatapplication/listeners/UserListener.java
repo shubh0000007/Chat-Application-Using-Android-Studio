872dd95aee5755a6e07cdeb4c74df604cecb10d9
proig <proigamex@gmail.com>
2023-12-04 13:35:58 +0530

    First commit

---
+++ app/src/main/java/com/example/chatapplication/listeners/UserListener.java
@@ -0,0 +1,6 @@
+package com.example.chatapplication.listeners;
+
+import com.example.chatapplication.models.User;
+public interface UserListener {
+    void onUserClicked(User user);
+}
---
+++ app/src/main/java/com/example/chatapplication/models/ChatMessage.java
@@ -0,0 +1,9 @@
+package com.example.chatapplication.models;
+
+import java.util.Date;
+
+public class ChatMessage {
+    public String senderId, receiverId, message, dateTime;
+    public Date dateobject;
+    public String conversionId, conversionName, conversionImage;
+}
